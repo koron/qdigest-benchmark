2470c989e0bc4e6ad925fa4b485e875ca200670a
MURAOKA Taro <taro.muraoka@muraoka-design.com>
2013-08-21 12:29:20 +0900

    linked with redis-qdigest

--- src/main/java/net/kaoriya/qb/qdigest_benchmark/Main.java
+++ src/main/java/net/kaoriya/qb/qdigest_benchmark/Main.java
@@ -1,8 +1,46 @@
 package net.kaoriya.qb.qdigest_benchmark;
 
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+import net.kaoriya.qb.redis_qdigest.QDigest;
+
 public class Main
 {
+    public static void benchmark(Jedis jedis) throws Exception
+    {
+        System.out.println("benchmark(Jedis)");
+        QDigest.dropInstance(jedis, "foo");
+        QDigest qd = QDigest.getInstance(jedis, "foo", 20);
+        qd.offer(10);
+        qd.offer(20, 30, 40, 50);
+        System.out.println("qd(0.0f)=" + qd.quantile(0.0f));
+        System.out.println("qd(0.5f)=" + qd.quantile(0.5f));
+        System.out.println("qd(1.0f)=" + qd.quantile(1.0f));
+    }
+
+    public static void benchmarkRedisQDigest(String host)
+    {
+        JedisPool pool = new JedisPool(new JedisPoolConfig(), host);
+        Jedis jedis = pool.getResource();
+        try {
+            benchmark(jedis);
+        } catch (RuntimeException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            pool.returnResource(jedis);
+        }
+        pool.destroy();
+    }
+
+    public static void executeBenchmark(String host) {
+        benchmarkRedisQDigest(host);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        executeBenchmark("127.0.0.1");
     }
 }
